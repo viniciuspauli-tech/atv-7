793a8b75981d52489c88d6ecd99cc5f6ec0c0c75
vinicius <vinicius_pauli@estudante.sesisenai.org.br>
2026-08-17 11:45:52 -0300

    add- database.php

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "sistema_pratos";
+
+try {
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar com o banco de dados: " . $e->getMessage());
+}
+?>
