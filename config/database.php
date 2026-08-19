a755d55b394541782f93128c0073b7ea7a0f1e6b
vinicius <vinicius_pauli@estudante.sesisenai.org.br>
2026-08-19 11:47:17 -0300

    att database

--- config/database.php
+++ config/database.php
@@ -1,16 +1,13 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "atv-7";
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "atv-7";
+    $conn = mysqli_connect($host, $user, $password, $database);
 
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("Erro ao conectar com o banco de dados: " . $conn->connect_error);
-}
-
-$conn->set_charset("utf8");
 ?>
