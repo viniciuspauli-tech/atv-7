96c4313e1bd62ea1d10cd3f9941f06dc448664f0
vinicius <vinicius_pauli@estudante.sesisenai.org.br>
2026-08-19 09:53:05 -0300

    add- atualizei o database e o header

--- header.php
+++ header.php
@@ -1,8 +1,3 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -13,17 +8,10 @@ if (session_status() === PHP_SESSION_NONE) {
 </head>
 <body>
     <header>
-        <h1> Sistema de Pratos</h1>
+        <h1>🍽️ Cadastro de Pratos</h1>
         <nav>
-            <?php if (isset($_SESSION['usuario_id'])): ?>
-                <a href="/sistema-pratos/index.php">Início</a>
-                <a href="/sistema-pratos/pratos/cadastrar.php">Cadastrar Prato</a>
-                <span>Olá, <?= htmlspecialchars($_SESSION['usuario_nome']); ?>!</span>
-                <a href="/sistema-pratos/logout.php" class="btn-sair">Sair</a>
-            <?php else: ?>
-                <a href="/sistema-pratos/login.php">Login</a>
-                <a href="/sistema-pratos/cadastro.php">Criar Conta</a>
-            <?php endif; ?>
+            <a href="/sistema-pratos/index.php">Ver Pratos</a>
+            <a href="/sistema-pratos/pratos/cadastrar.php">Cadastrar Novo Prato</a>
         </nav>
     </header>
     <main class="container">
